a6302e225b44b21f827a6e6ca99bb2541a62fed7
Lushnikova, Alvina <lushnalv@fel.cvut.cz>
2022-10-05 18:23:32 +0200

    Initial commit

--- src/main/kotlin/com/example/messenger/MessengerApplication.kt
+++ src/main/kotlin/com/example/messenger/MessengerApplication.kt
@@ -3,6 +3,7 @@ package com.example.messenger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class MessengerApplication
